f50b7d9feee389cafbd9e8706fcbaa92ae894ce2
Bruno Petrus <bruno.petrus@live.com>
2015-12-17 21:12:42 +0100

    Hare class completed
    Hare class is completed, I think.

--- Hare.java
+++ Hare.java
@@ -48,15 +48,38 @@ public class Hare {
 	//	Update functions
 	//--------------------------------
 	public void move() {
+		//Generate random number
+		int rNumber = generateNumber(1, 10);
 		
-	}
-	
-	public boolean victory() {
-		if ()
+		//Move
+		//1 or 2 -> Big hop, move 9 squares to right
+		if (rNumber == 1 || rNumber == 2) {
+			position += 6;
+		}
+		//3 4 5 -> Small hop, move 1 square to right
+		else if (rNumber >= 3 && rNumber <= 5) {
+			position += 1;
+		}
+		//6 -> Big slip, move 12 squares to left
+		else if(rNumber == 6) {
+			position -= 12;
+			if (position < 0)
+				position = 0;
+		}
+		//7 8 -> Small slip, move 2 squares to left
+		else if(rNumber == 7 || rNumber == 8) {
+			position -= 2;
+			if (position < 0)
+				position = 0;
+		}
+		//9 10 -> fall asleep, don't move
+		else {}
 	}
 	
 	//--------------------------------
 	//	Logic functions
 	//--------------------------------
-	
+	private int generateNumber(int MIN, int MAX) {
+		return MIN + (int)(Math.random() * ((MAX - MIN) + 1));
+	}
 }
--- HareAndTortoise.java
+++ HareAndTortoise.java
@@ -4,22 +4,35 @@ public class HareAndTortoise {
 		final int MAX = 10;
 		final int MIN = 1;
 		
-		for (int x = 0; x < 100; x++) {
+		/*for (int x = 0; x < 100; x++) {
 			if (x % 10 == 0)
 				System.out.println();
 			System.out.print((MIN + (int)(Math.random() * ((MAX - MIN) + 1))) + " ");
-		}
+		}*/
 		
 		//TODO Everything should be nested in a loop. So user can change the length of race.
 		
 		//Game loop
-		while(true) {
+		Hare hare = new Hare(0, 10);
+		int x = 0;
+		while(hare.getPosition() <= 50) {
 			//Update hare
+			hare.move();
 			
 			//Update tortoise
 			
 			//Print result
-			
+			System.out.println(hare.getPosition());
+			x++;
 		}
+		
+		System.out.println("Iterations: " + x);
 	}
+	
+	/*public static boolean victory() {
+		if (position >= lengthOfRace) {
+			return true;
+		}
+		return false;
+	}*/
 }
